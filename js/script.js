a29a37524387f2e7b67c3cb7ec551903a77bb128
Felipe <felipe.rtec@gmail.com>
2017-05-18 12:21:37 -0300

    jashfkasjhg

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert("funcionando");
+alert("O jogo irá começar!");
+
+function startGame(){
+    myGameArea.start();
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function(){
+          this.canvas.width = 400;
+          this.canvas.height = 270;
+          this.context = this.canvas.getContext("2d");
+          document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
